401130f6add635c283ec08803e1dc115ba5a3db6
Elizabeth <liz4momentum@gmail.com>
2023-06-26 10:10:47 -0600

    Added title, moved image to nav component

--- Madeline_Artwork.client/src/pages/HomePage.vue
+++ Madeline_Artwork.client/src/pages/HomePage.vue
@@ -1,12 +1,4 @@
-<template>
-  <div class="container-fluid">
-    <section class="row">
-      <div class="col-12 d-flex justify-content-center">
-        <img src="../assets/img/temp-logo.png" alt="a line drawing of mushrooms" class="hero-img">
-      </div>
-    </section>
-  </div>
-</template>
+<template></template>
 
 <script>
 export default {
@@ -16,10 +8,4 @@ export default {
 }
 </script>
 
-<style scoped lang="scss">
-.hero-img {
-  object-fit: cover;
-  object-position: center;
-  height: 40vh;
-}
-</style>
+<style scoped lang="scss"></style>
