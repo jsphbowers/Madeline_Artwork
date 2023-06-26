19921b7bc39cb2bc7491f0f013183476915b6649
Elizabeth <liz4momentum@gmail.com>
2023-06-26 10:13:53 -0600

    Adding router links to navbar

--- Madeline_Artwork.client/src/components/Navbar.vue
+++ Madeline_Artwork.client/src/components/Navbar.vue
@@ -5,8 +5,9 @@
         <img src="../assets/img/temp-logo.png" alt="a line drawing of mushrooms" class="hero-img">
         <h1 class="cursive">New Earth Designs</h1>
       </div>
-      <div class="col-10">
-        <!-- insert new nav component here -->
+      <div class="col-10 d-flex justify-content-between">
+        <router-link :to="{ name: 'Home' }" class="text-dark">Home</router-link>
+        <router-link :to="{ name: 'About' }" class="text-dark">About</router-link>
       </div>
     </section>
   </div>
